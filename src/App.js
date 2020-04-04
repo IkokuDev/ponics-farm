1f636150d98ddb75734e0bf427512964ebec084f
Michael <vellidevv@gmail.com>
2020-04-04 16:32:17 -0700

    updated app with shop page, collection item and collection preview

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
-const OilsPage = () =>(
-  <h1>OIL PAGE</h1>
-)
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path = '/' component = {HomePage} />
-      <Route path = '/oils' component = {OilsPage} />
+      <Route path = '/shop' component = {ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,206 @@
+const SHOP_DATA = [
+    {
+      id: 1,
+      title: 'Oils',
+      routeName: 'oils',
+      items: [
+        {
+          id: 1,
+          name: 'Coconut Oil',
+          imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+          price: 2500
+        },
+        {
+          id: 2,
+          name: 'Castor Oil',
+          imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+          price: 1800
+        },
+        {
+          id: 3,
+          name: 'Black Seed Oil',
+          imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+          price: 3500
+        },
+        {
+          id: 4,
+          name: 'Avocado Oil',
+          imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+          price: 2500
+        },
+        {
+          id: 5,
+          name: 'Tea Tree Oil',
+          imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
+          price: 1800
+        },
+        {
+          id: 6,
+          name: 'Olive Oil',
+          imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
+          price: 1400
+        },
+        {
+          id: 7,
+          name: 'Almond Oil',
+          imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
+          price: 1800
+        },
+        {
+          id: 8,
+          name: 'Shea Butter',
+          imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
+          price: 1400
+        },
+        {
+          id: 9,
+          name: 'Palm Oil',
+          imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
+          price: 1600
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: 'Spices',
+      routeName: 'spices',
+      items: [
+        {
+          id: 1,
+          name: 'Corriander',
+          imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+          price: 2200
+        },
+        {
+          id: 2,
+          name: 'Thyme',
+          imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
+          price: 2800
+        },
+        {
+          id: 3,
+          name: 'Parsley',
+          imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
+          price: 1100
+        },
+        {
+          id: 4,
+          name: 'Basil',
+          imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
+          price: 160
+        },
+        {
+          id: 5,
+          name: 'Salt',
+          imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
+          price: 1600
+        },
+        {
+          id: 6,
+          name: 'Rosemary',
+          imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
+          price: 1600
+        },
+        {
+          id: 7,
+          name: 'Chives',
+          imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
+          price: 1900
+        },
+        {
+          id: 8,
+          name: 'Black Pepper',
+          imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
+          price: 200
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: 'Flowers',
+      routeName: 'flowers',
+      items: [
+        {
+          id: 1,
+          name: 'Hibiscus',
+          imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+          price: 1250
+        },
+        {
+          id: 2,
+          name: 'Sunflower',
+          imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
+          price: 900
+        },
+        {
+          id: 3,
+          name: 'Rose',
+          imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
+          price: 900
+        },
+        {
+          id: 4,
+          name: 'Tulip',
+          imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
+          price: 1650
+        },
+        {
+          id: 5,
+          name: 'Nightshade',
+          imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
+          price: 1850
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: 'Vegetables',
+      routeName: 'vegetables',
+      items: [
+        {
+          id: 1,
+          name: 'Celery',
+          imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+          price: 2500
+        },
+        {
+          id: 2,
+          name: 'Lettuce',
+          imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
+          price: 2000
+        },
+        {
+          id: 3,
+          name: 'Spinach',
+          imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
+          price: 800
+        },
+        {
+          id: 4,
+          name: 'Cucumber',
+          imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
+          price: 800
+        },
+        {
+          id: 5,
+          name: 'Ugwu',
+          imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
+          price: 450
+        },
+        {
+          id: 6,
+          name: 'Tomatoes',
+          imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
+          price: 1350
+        },
+        {
+          id: 7,
+          name: 'Pepper',
+          imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
+          price: 200
+        }
+      ]
+    }
+    
+  ]
+  export default SHOP_DATA;
